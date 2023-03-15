1b49e3c72163def51d74dd039e5496cde2d45922
Ivan Vedernikov <ecugol@gmail.com>
2023-03-15 17:30:57 +0000

    fix: django.urls import

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from django.conf.urls import include, re_path
+from django.urls import include, re_path
 from django.contrib import admin
 from django.conf import settings
 
@@ -24,7 +24,7 @@ if 'test' not in sys.argv:
 
 # For security reason
 if settings.DEBUG:
-    from django.conf.urls.static import static
+    from django.urls.static import static
 
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
